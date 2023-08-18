5c16af875141154a65cd144c5924f52b9e3073a8
AdnanSaif322 <adnan.saif.322@gmail.com>
2023-08-18 20:21:41 +0600

    error handling

--- server/app.js
+++ server/app.js
@@ -29,6 +29,19 @@ app.get("/login", (req, res) => {
   res.send("login page");
 });
 
+// client error handling
+app.use((req,res,next)=>{
+  res.status(404).json({message: "route not found"});
+  next();
+})
+
+// server error handling
+app.use((err,req,next)=>{
+  console.error(err.stack);
+  res.status(500).send('something broke!');
+  
+})
+
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
